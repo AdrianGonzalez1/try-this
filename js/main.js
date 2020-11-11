0fb2fee6ffa24efde8ae2229d76cc8654d825db3
Adrian Gonzalez <adrian131@me.com>
2020-11-11 10:36:39 -0800

    fixed merge conflict

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var goButton = document.querySelector('.go-button');
+
+
+
+goButton.addEventListener('click', function () {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://www.boredapi.com/api/activity/' )
+  xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+    console.log(xhr.response)
+  });
+  xhr.send();
+})
